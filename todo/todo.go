2ab799965c39898eae580b54e4cb46424667851c
Shubh Pingale <135618936+shubhtrek@users.noreply.github.com>
2026-02-18 08:40:37 +0530

    Func LoadTODO

--- todo/todo.go
+++ todo/todo.go
@@ -1,7 +1,30 @@
 package todo
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Todo struct {
 	ID    int    `json:"id"`
 	Title string `json:"title"`
 	Done  bool   `json:"done"`
+}
+
+func LoadTodos(filename string) ([]Todo, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Todo{}, nil
+		}
+		return nil, err
+	}
+
+	if len(data) == 0 {
+		return []Todo{}, nil
+	}
+
+	var todos []Todo
+	err = json.Unmarshal(data, &todos)
+	return todos, err
 }
