4132f9c75634879e098b9d5c1e110056a095d81a
Shubh Pingale <135618936+shubhtrek@users.noreply.github.com>
2026-02-23 18:01:24 +0530

    Interface

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"go-todo-cli/todo"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const dataFile = "data.json"
 
 func main() {
-	fmt.Println("GO-CLI-TODO")
+	reader := bufio.NewReader(os.Stdin)
+
+	todos, err := todo.LoadTodos(dataFile)
+	if err != nil {
+		fmt.Println("❌ Error loading todos:", err)
+		return
+	}
+
+	for {
+		fmt.Println("\n📝 Todo CLI App")
+		fmt.Println("1. Add Todo")
+		fmt.Println("2. List Todos")
+		fmt.Println("3. Mark Done")
+		fmt.Println("4. Delete Todo")
+		fmt.Println("5. Exit")
+		fmt.Print("Choose option: ")
+
+		choiceStr, _ := reader.ReadString('\n')
+		choiceStr = strings.TrimSpace(choiceStr)
+		choice, _ := strconv.Atoi(choiceStr)
+
+	}
 }
